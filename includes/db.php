d2c69464e6cf475e07c89f3d622d555dd89bd9e8
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-12 11:02:23 +0330

    Inserting Data into Category Table and Displaying it

--- includes/db.php
+++ includes/db.php
@@ -11,6 +11,6 @@ foreach ($db as $key => $value){
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
-if ($connection){
-    echo "connect to db";
-}
+//if ($connection){
+//    echo "connect to db";
+//}
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include "includes/db.php"; ?>
 <!--Header-->
 <?php include "includes/header.php"; ?>
     <!-- Navigation -->

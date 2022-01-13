234e7439a5a7a419d985e37ac79d6b68d8132625
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-13 21:11:53 +0330

    Updating or Editing Categories part 1

--- admin/categories.php
+++ admin/categories.php
@@ -26,7 +26,7 @@
                         } else {
                             $query = "INSERT INTO categories(`cat_title`) ";
                             $query .= "VALUE('{$category_title}')";
-                            $create_category = mysqli_query($connection , $query);
+                            $create_category = mysqli_query($connection, $query);
                             header("Location: categories.php");
                             if (!$create_category) {
                                 die("something wrong!" . mysqli_error($connection));
@@ -41,6 +41,28 @@
                         </div>
                         <div class="form-group">
                             <input type="submit" class="btn btn-primary" name="submit" value="Add category">
+                        </div>
+                    </form>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="cat-title">Edit Category</label>
+                            <?php
+                            if (isset($_GET['edit'])) {
+                                $cat_id = $_GET['edit'];
+                                $query = "SELECT * FROM categories WHERE cat_id = {$cat_id}";
+                                $select_categories = mysqli_query($connection, $query);
+                                while ($row = mysqli_fetch_assoc($select_categories)) {
+                                    $cat_id = $row['cat_id'];
+                                    $cat_title = $row['cat_title'];
+                                    ?>
+                                    <input type="text" value="<?php if (isset($cat_title)){echo $cat_title;} ?>" class="form-control" name="cat_title">
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" name="submit" value="Update category">
                         </div>
                     </form>
                 </div>
@@ -63,16 +85,17 @@
                                         <td>{$cat_id}</td>
                                         <td>{$cat_title}</td>
                                         <td><a href='categories.php?delete={$cat_id}' class='btn btn-sm btn-danger'>Delete</a></td>
+                                        <td><a href='categories.php?edit={$cat_id}' class='btn btn-sm btn-primary'>Edit</a></td>
                                     </tr>";
                         }
                         ?>
                         </tbody>
 
                         <?php
-                        if (isset($_GET['delete'])){
+                        if (isset($_GET['delete'])) {
                             $getCatID = $_GET['delete'];
                             $query = "DELETE FROM categories WHERE cat_id = {$getCatID}";
-                            $deleteCategory = mysqli_query($connection , $query);
+                            $deleteCategory = mysqli_query($connection, $query);
                             header("Location: categories.php");
                         }
                         ?>

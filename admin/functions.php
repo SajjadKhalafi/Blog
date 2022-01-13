1f7a31874f11fc105e0da0e9b462664cd2c1f39a
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-13 21:54:06 +0330

    Refactoring category code part 2

--- admin/functions.php
+++ admin/functions.php
@@ -17,3 +17,31 @@ function insert_category()
         }
     }
 }
+
+function selectAllCategories()
+{
+    global $connection;
+    $query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        echo "<tr>
+                                        <td>{$cat_id}</td>
+                                        <td>{$cat_title}</td>
+                                        <td><a href='categories.php?delete={$cat_id}' class='btn btn-sm btn-danger'>Delete</a>
+                                        <a href='categories.php?edit={$cat_id}' class='btn btn-sm btn-primary'>Edit</a></td>
+                                    </tr>";
+    }
+}
+
+function deleteCategory()
+{
+    global $connection;
+    if (isset($_GET['delete'])) {
+        $getCatID = $_GET['delete'];
+        $query = "DELETE FROM categories WHERE cat_id = {$getCatID}";
+        $deleteCategory = mysqli_query($connection, $query);
+        header("Location: categories.php");
+    }
+}

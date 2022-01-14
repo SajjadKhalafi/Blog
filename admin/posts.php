158e32841be795299eab271c5dcd5875860a3099
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-14 15:04:26 +0330

    Displaying posts list in admin

--- admin/posts.php
+++ admin/posts.php
@@ -32,17 +32,32 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>10</td>
-                                <td>Edwin Diaz</td>
-                                <td>Bootstrap Framework</td>
-                                <td>Bootstrap</td>
-                                <td>Status</td>
-                                <td>Image</td>
-                                <td>Tags</td>
-                                <td>Comments</td>
-                                <td>Date</td>
-                            </tr>
+                        <?php
+                        $query = "SELECT * FROM posts";
+                        $select_posts = mysqli_query($connection, $query);
+                        while ($row = mysqli_fetch_assoc($select_posts)) {
+                            $post_id = $row['post_id'];
+                            $post_author = $row['post_author'];
+                            $post_title = $row['post_title'];
+                            $post_category_id = $row['post_category_id'];
+                            $post_status = $row['post_status'];
+                            $post_image = $row['post_image'];
+                            $post_tags = $row['post_tags'];
+                            $post_comments_count = $row['post_comment_count'];
+                            $post_date = $row['post_date'];
+                            echo "<tr>
+                                      <td>{$post_id}</td>
+                                      <td>{$post_author}</td>
+                                      <td>{$post_title}</td>
+                                      <td>{$post_category_id}</td>
+                                      <td>{$post_status}</td>
+                                      <td><img src='../images/{$post_image}' width='100'> </td>
+                                      <td>{$post_tags}</td>
+                                      <td>{$post_comments_count}</td>
+                                      <td>{$post_date}</td>
+                                  </tr>";
+                        }
+                        ?>
                         </tbody>
                     </table>
                 </div>

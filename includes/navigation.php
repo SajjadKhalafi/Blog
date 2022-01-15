3531f289dd71aa2703b1aca54a71dbf307911648
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-15 17:20:34 +0330

    Setting up some links and making an excerpt

--- includes/navigation.php
+++ includes/navigation.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+            <a class="navbar-brand" href="index.php">CMS</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -18,7 +18,8 @@
                 $select_all_categories_query = mysqli_query($connection , $query);
                 while($row = mysqli_fetch_assoc($select_all_categories_query)){
                     $cat_title = $row['cat_title'];
-                    echo "<li><a href='#'>{$cat_title}</a></li>";
+                    $cat_id = $row['cat_id'];
+                    echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                 }
                 ?>
                 <li><a href='admin/'>Admin</a></li>

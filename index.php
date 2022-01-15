a032794ff173b84fbc954e25b7b35ae613ac39d0
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-15 16:58:05 +0330

    Adding Individual Post Page and Link

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
                 $query = "SELECT * FROM posts";
                 $select_all_posts_query = mysqli_query($connection , $query);
                 while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                    $post_id = $row['post_id'];
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -29,7 +30,7 @@
 
                     <!-- First Blog Post -->
                     <h2>
-                        <a href="#"><?= $post_title ?></a>
+                        <a href="post.php?p_id=<?= $post_id ?>"><?= $post_title ?></a>
                     </h2>
                     <p class="lead">
                         by <a href="index.php"><?= $post_author ?></a>

0b16ce1809464b2035ef3debe6e8d4a5d397fc66
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-15 17:10:16 +0330

    Creating the category page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -29,8 +29,9 @@
                     $query = "SELECT * FROM categories";
                     $select_categories_sidebar = mysqli_query($connection , $query);
                     while($row = mysqli_fetch_assoc($select_categories_sidebar)){
+                        $cat_id = $row['cat_id'];
                         $cat_title = $row['cat_title'];
-                        echo "<li><a href='#'>{$cat_title}</a></li>";
+                        echo "<li><a href='category.php?category=$cat_id'>{$cat_title}</a></li>";
                     }
                     ?>
                 </ul>

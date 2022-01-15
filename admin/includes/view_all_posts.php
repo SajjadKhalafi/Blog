3b15c73594d4d5b3e7e9af842e8f8e1ef1e6310d
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-15 11:24:41 +0330

    Relating Categories to posts and Displaying it

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -26,27 +26,36 @@
         $post_tags = $row['post_tags'];
         $post_comments_count = $row['post_comment_count'];
         $post_date = $row['post_date'];
-        echo "<tr>
-                                      <td>{$post_id}</td>
-                                      <td>{$post_author}</td>
-                                      <td>{$post_title}</td>
-                                      <td>{$post_category_id}</td>
-                                      <td>{$post_status}</td>
-                                      <td><img src='../images/{$post_image}' width='100'> </td>
-                                      <td>{$post_tags}</td>
-                                      <td>{$post_comments_count}</td>
-                                      <td>{$post_date}</td>
-                                      <td><a href='posts.php?source=edit_post&p_id=$post_id' class='btn btn-primary btn-sm'>Edit</a></td>
-                                      <td><a href='posts.php?delete=$post_id' class='btn btn-danger btn-sm'>Delete</a></td>
-                                  </tr>";
+
+        echo "<tr>";
+        echo "<td>{$post_id}</td>";
+        echo "<td>{$post_author}</td>";
+        echo "<td>{$post_title}</td>";
+
+        // display categoey title
+        $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
+        $select_categories = mysqli_query($connection, $query);
+        while ($row = mysqli_fetch_assoc($select_categories)) {
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title'];
+            echo "<td>{$cat_title}</td>";
+        }
+        echo "<td>{$post_status}</td>";
+        echo "<td><img src='../images/{$post_image}' width='100'> </td>";
+        echo "<td>{$post_tags}</td>";
+        echo "<td>{$post_comments_count}</td>";
+        echo "<td>{$post_date}</td>";
+        echo "<td><a href='posts.php?source=edit_post&p_id=$post_id' class='btn btn-primary btn-sm'>Edit</a></td>";
+        echo "<td><a href='posts.php?delete=$post_id' class='btn btn-danger btn-sm'>Delete</a></td>";
+        echo "</tr>";
     }
     ?>
 
     <?php
-    if (isset($_GET['delete'])){
+    if (isset($_GET['delete'])) {
         $post_id = $_GET['delete'];
         $query = "DELETE FROM posts WHERE post_id = $post_id";
-        $deletePost = mysqli_query($connection , $query);
+        $deletePost = mysqli_query($connection, $query);
         header("Location: posts.php");
     }
     ?>

498c72ed0d5b414e9087558a3f14685005829ea3
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-16 13:03:48 +0330

    Adjustments for comments and Displaying Post Based on Status

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
 <?php include "includes/db.php"; ?>
 <!--Header-->
 <?php include "includes/header.php"; ?>
-    <!-- Navigation -->
+<!-- Navigation -->
 <?php include "includes/navigation.php"; ?>
 
 
-    <!-- Page Content -->
-    <div class="container">
+<!-- Page Content -->
+<div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-                <?php
-                $query = "SELECT * FROM posts";
-                $select_all_posts_query = mysqli_query($connection , $query);
-                while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                    $post_id = $row['post_id'];
-                    $post_title = $row['post_title'];
-                    $post_author = $row['post_author'];
-                    $post_date = $row['post_date'];
-                    $post_image = $row['post_image'];
-                    $post_content = substr($row['post_content'],0,100);
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <?php
+            $query = "SELECT * FROM posts";
+            $select_all_posts_query = mysqli_query($connection, $query);
+            while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                $post_id = $row['post_id'];
+                $post_title = $row['post_title'];
+                $post_author = $row['post_author'];
+                $post_date = $row['post_date'];
+                $post_image = $row['post_image'];
+                $post_content = substr($row['post_content'], 0, 100);
+                $post_status = $row['post_status'];
+
+                if ($post_status !== 'published') {
+                    echo "<h1 class='text-center'>No Post Sorry</h1>";
+                } else {
+
+
                     ?>
                     <h1 class="page-header">
                         Page Heading
@@ -40,23 +47,25 @@
                     <img class="img-responsive" src="images/<?= $post_image ?>" alt="">
                     <hr>
                     <p><?= $post_content ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <a class="btn btn-primary" href="#">Read More <span
+                                class="glyphicon glyphicon-chevron-right"></span></a>
 
                     <hr>
 
                     <?php
                 }
-                ?>
-
-            </div>
-
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include "includes/sidebar.php";?>
+            }
+            ?>
 
         </div>
-        <!-- /.row -->
 
-        <hr>
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php"; ?>
 
-        <!-- Footer -->
-        <?php include "includes/footer.php";?>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+
+    <!-- Footer -->
+    <?php include "includes/footer.php"; ?>

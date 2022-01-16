baa7a9a485a85fbe6a6537fe738744225e5f5bb4
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-16 22:58:29 +0330

    display posts order by DESC in admin section

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -16,7 +16,7 @@
     </thead>
     <tbody>
     <?php
-    $query = "SELECT * FROM posts";
+    $query = "SELECT * FROM posts ORDER BY post_id DESC";
     $select_posts = mysqli_query($connection, $query);
     while ($row = mysqli_fetch_assoc($select_posts)) {
         $post_id = $row['post_id'];

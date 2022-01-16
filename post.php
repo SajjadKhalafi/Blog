bea633b6bd107d0f1991ce99e6bdf0cf7f44bcd6
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-16 11:56:45 +0330

    Displaying comments based on approval

--- post.php
+++ post.php
@@ -67,6 +67,7 @@
                 if (!$create_comment_query){
                     die("QUERY FAILED" . mysqli_error($connection));
                 }
+                header("Location: post.php?p_id=$post_id");
             }
             ?>
             <div class="well">
@@ -92,49 +93,33 @@
             <!-- Posted Comments -->
 
             <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
-                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                    vulputate fringilla. Donec lacinia congue felis in faucibus.
-                </div>
-            </div>
-
-            <!-- Comment -->
-            <div class="media">
-                <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Start Bootstrap
-                        <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
-                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
-                    vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    <!-- Nested Comment -->
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-                            commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
-                            condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
+            <?php
+            $query = "SELECT * FROM comments WHERE comment_post_id = $post_id ";
+            $query .= "AND comment_status = 'Approved' ";
+            $query .= "ORDER BY comment_id DESC";
+            $select_all_comments = mysqli_query($connection , $query);
+            if (!$select_all_comments){
+                die("QUERY FAILED " . mysqli_error($connection));
+            }
+            while ($row = mysqli_fetch_assoc($select_all_comments)){
+                $comment_author = $row['comment_author'];
+                $comment_date = $row['comment_date'];
+                $comment_content = $row['comment_content'];
+                ?>
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><?= $comment_author; ?>
+                            <small><?= $comment_date; ?></small>
+                        </h4>
+                        <?= $comment_content; ?>
                     </div>
-                    <!-- End Nested Comment -->
                 </div>
-            </div>
+            <?php } ?>
+
+
         </div>
 
         <!-- Blog Sidebar Widgets Column -->

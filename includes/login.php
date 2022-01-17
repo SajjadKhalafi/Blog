a4ce2cc678cbf358b28a37c5a237dfe4615888ba
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-17 20:54:13 +0330

    Select User Query

--- includes/login.php
+++ includes/login.php
@@ -4,4 +4,16 @@ include "db.php";
 if (isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection , $username);
+    $password = mysqli_real_escape_string($connection , $password);
+
+    $query = "SELECT * FROM users WHERE username = '$username'";
+    $select_query = mysqli_query($connection , $query);
+    if (!$select_query){
+        die("QUERY FAILED " . mysqli_error($connection));
+    }
+    while ($row = mysqli_fetch_array($select_query)){
+        echo $row['user_id'];
+    }
 }

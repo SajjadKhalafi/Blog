ae41464081ab74ff9a77176415c88f1a6648e367
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-18 09:14:18 +0330

    Validating User Admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -2,6 +2,13 @@
 <?php session_start() ?>
 <?php include "functions.php";?>
 <?php include "../includes/db.php";?>
+<?php
+if (isset($_SESSION['user_role'])){
+    if ($_SESSION['user_role'] !== 'admin'){
+        header("Location: ../index.php");
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 

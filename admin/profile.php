659a8ec67bfb19eba9c6379c8e29efd38ee8fa3d
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-19 11:53:04 +0330

    Updating User Data

--- admin/profile.php
+++ admin/profile.php
@@ -15,6 +15,33 @@ if(isset($_SESSION['username'])){
         $user_role = $row['user_role'];
     }
 }
+
+if (isset($_POST['edit_user'])) {
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    $username = $_POST['username'];
+
+//    $image_name = $_FILES['image']['name'];
+//    $image_tmp_name = $_FILES['image']['tmp_name'];
+
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+//    $post_date = date("d-m-y");
+
+
+    $query = "UPDATE users SET ";
+    $query .= "user_firstname = '$user_firstname' , ";
+    $query .= "user_lastname = '$user_lastname' , ";
+    $query .= "user_role = '$user_role' , ";
+    $query .= "username = '$username' , ";
+    $query .= "user_email = '$user_email' , ";
+    $query .= "user_password = '$user_password' ";
+    $query .= "WHERE username = '$username'";
+    $edit_user_query = mysqli_query($connection, $query);
+    confirmQuery($edit_user_query);
+}
+
 ?>
     <div id="wrapper">
 
@@ -73,7 +100,7 @@ if(isset($_SESSION['username'])){
                         <input type="password" class="form-control" name="user_password" value="<?= $user_password ?>">
                     </div>
                     <div class="form-group">
-                        <input type="submit" class="btn btn-primary" name="edit_user" value="Update User">
+                        <input type="submit" class="btn btn-primary" name="edit_user" value="Update Profile">
                     </div>
                 </form>
 

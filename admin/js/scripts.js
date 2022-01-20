d81d3d206d500ff2512f1edd10f91602aede569e
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-20 16:05:33 +0330

    Create scripts file for admin

---
+++ admin/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    ClassicEditor
+        .create( document.querySelector( '#body' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+});

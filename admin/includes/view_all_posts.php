ea0d4086fa53952895479b666dfb846e5a4875ef
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-22 10:49:12 +0330

    Adding Link to Add New Button in Post Page

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -36,7 +36,7 @@ if (isset($_POST['checkboxArray'])) {
 
     <div class="col-xs-4">
         <input type="submit" name="submit" class="btn btn-success" value="Apply">
-        <a href="add_post.php" class="btn btn-primary">Add New</a>
+        <a href="posts.php?source=add_post" class="btn btn-primary">Add New</a>
     </div>
 
     <table class="table table-bordered table-hover">

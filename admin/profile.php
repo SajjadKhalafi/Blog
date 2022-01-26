b907a605edf576ebe430150a3cd0c18a6713315e
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-26 10:38:50 +0330

    Cleaning Up Profile Page

--- admin/profile.php
+++ admin/profile.php
@@ -7,7 +7,6 @@ if(isset($_SESSION['username'])){
     while($row = mysqli_fetch_array($select_user_profile_query)){
         $user_id = $row['user_id'];
         $username = $row['username'];
-        $user_password = $row['user_password'];
         $user_firstname = $row['user_firstname'];
         $user_lastname = $row['user_lastname'];
         $user_email = $row['user_email'];
@@ -21,14 +20,10 @@ if (isset($_POST['edit_user'])) {
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
     $username = $_POST['username'];
-
-//    $image_name = $_FILES['image']['name'];
-//    $image_tmp_name = $_FILES['image']['tmp_name'];
-
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
-//    $post_date = date("d-m-y");
 
+    $user_password = password_hash($user_password , PASSWORD_BCRYPT , ['cost' => 10]);
 
     $query = "UPDATE users SET ";
     $query .= "user_firstname = '$user_firstname' , ";
@@ -65,40 +60,27 @@ if (isset($_POST['edit_user'])) {
                         <label for="">Firstname</label>
                         <input type="text" class="form-control" name="user_firstname" value="<?= $user_firstname ?>">
                     </div>
+
                     <div class="form-group">
                         <label for="">Lastname</label>
                         <input type="text" class="form-control" name="user_lastname" value="<?= $user_lastname ?>">
-                    </div>
-                    <div class="form-group">
-                        <select name="user_role" id="">
-                            <option value="<?= $user_role;?>"><?= $user_role ?></option>
-                            <?php
-                            if ($user_role === "admin"){
-                                echo "<option value='subscriber'>subscriber</option>";
-                            }else{
-                                echo "<option value='admin'>admin</option>";
-                            }
-                            ?>
-
-                        </select>
                     </div>
 
                     <div class="form-group">
                         <label for="">Username</label>
                         <input type="text" class="form-control" name="username" value="<?= $username ?>">
                     </div>
-                    <!--    <div class="form-group">-->
-                    <!--        <label for="">Post Author</label>-->
-                    <!--        <input type="file" name="image">-->
-                    <!--    </div>-->
+
                     <div class="form-group">
                         <label for="">Email</label>
                         <input type="email" class="form-control" name="user_email" value="<?= $user_email?>">
                     </div>
+
                     <div class="form-group">
                         <label for="">Password</label>
-                        <input type="password" class="form-control" name="user_password" value="<?= $user_password ?>">
+                        <input autocomplete="off" type="password" class="form-control" name="user_password">
                     </div>
+
                     <div class="form-group">
                         <input type="submit" class="btn btn-primary" name="edit_user" value="Update Profile">
                     </div>

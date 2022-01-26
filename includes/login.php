d350fb4ae2548f9b1c81f04e8fa978226bdcead8
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-26 05:34:15 +0330

    Login in Users with new System

--- includes/login.php
+++ includes/login.php
@@ -21,9 +21,9 @@ if (isset($_POST['login'])){
         $db_user_password = $row['user_password'];
         $db_user_role = $row['user_role'];
     }
-    $password = crypt($password, $db_user_password);
+//    $password = crypt($password, $db_user_password);
 
-    if ($username === $db_username && $password === $db_user_password){
+    if ($username === $db_username && password_verify($password , $db_user_password)){
         $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
         $_SESSION['lastname'] = $db_user_lastname;

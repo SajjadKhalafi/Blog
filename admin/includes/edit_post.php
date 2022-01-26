3c13a67c0f96ad9ab21af99ffaeeebcf29cbf4ba
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-26 11:45:53 +0330

    adding real escape string for post data in database in edit_post page

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -20,14 +20,14 @@ while ($row = mysqli_fetch_assoc($select_post_by_id)) {
 
 <?php
 if (isset($_POST['update_post'])){
-    $post_title = $_POST['post_title'];
+    $post_title = mysqli_real_escape_string($connection , $_POST['post_title']);
     $post_category_id = $_POST['post_category'];
-    $post_author = $_POST['post_author'];
+    $post_author = mysqli_real_escape_string($connection , $_POST['post_author']);
     $post_status = $_POST['post_status'];
     $post_image = $_FILES['post_image']['name'];
     $post_image_tmp = $_FILES['post_image']['tmp_name'];
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
+    $post_tags = mysqli_real_escape_string($connection , $_POST['post_tags']);
+    $post_content = mysqli_real_escape_string($connection , $_POST['post_content']);
 
     move_uploaded_file($post_image_tmp , "../images/$post_image");
 

3a359fac7b42021caf982455f7528fb83cdb8338
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-01-26 10:56:47 +0330

    Cleaning Up Profile Page

--- admin/profile.php
+++ admin/profile.php
@@ -28,7 +28,6 @@ if (isset($_POST['edit_user'])) {
     $query = "UPDATE users SET ";
     $query .= "user_firstname = '$user_firstname' , ";
     $query .= "user_lastname = '$user_lastname' , ";
-    $query .= "user_role = '$user_role' , ";
     $query .= "username = '$username' , ";
     $query .= "user_email = '$user_email' , ";
     $query .= "user_password = '$user_password' ";

fc0ba8e35f4e0084817405bc1cb3be8e816c21c9
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-02-06 07:55:09 +0330

    Create escape function

--- admin/functions.php
+++ admin/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function escape($string)
+{
+    global $connection;
+    return mysqli_real_escape_string($connection , trim($string));
+}
+
 function users_online()
 {
     if (isset($_GET['onlineUsers'])){

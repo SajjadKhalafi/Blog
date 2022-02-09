2d733dfeeed8adc5906e5121457314bcfcae9136
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-02-09 12:22:05 +0330

    Small body font & adding text-center class

--- admin/includes/delete_modal.php
+++ admin/includes/delete_modal.php
@@ -1,6 +1,3 @@
-<!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
@@ -12,7 +9,7 @@
                 <h4 class="modal-title">Delete Post</h4>
             </div>
             <div class="modal-body">
-                <h2>Are You Sure, You want to delete this post?</h2>
+                <h3 class="text-center">Are You Sure, You want to delete this post?</h3>
             </div>
             <div class="modal-footer">
                 <a href="" class="btn btn-danger delete_modal">Delete</a>

ea78cf828e2c56c3a48ea985d38203b90b51e4c2
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-02-14 10:46:09 +0330

    Refactoring index in admin part 1

--- admin/index.php
+++ admin/index.php
@@ -30,13 +30,7 @@
                                     <i class="fa fa-file-text fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <?php
-                                    $query = "SELECT * FROM posts";
-                                    $select_all_posts = mysqli_query($connection, $query);
-                                    $post_count = mysqli_num_rows($select_all_posts);
-                                    echo "<div class='huge'>{$post_count}</div>";
-                                    ?>
-
+                                    <div class='huge'><?= $post_count = recordCount('posts'); ?></div>
                                     <div>Posts</div>
                                 </div>
                             </div>
@@ -58,12 +52,7 @@
                                     <i class="fa fa-comments fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <?php
-                                    $query = "SELECT * FROM comments";
-                                    $select_all_comments = mysqli_query($connection, $query);
-                                    $comment_count = mysqli_num_rows($select_all_comments);
-                                    echo "<div class='huge'>{$comment_count}</div>";
-                                    ?>
+                                    <div class='huge'><?= $comment_count = recordCount('comments'); ?></div>
                                     <div>Comments</div>
                                 </div>
                             </div>
@@ -85,12 +74,7 @@
                                     <i class="fa fa-user fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <?php
-                                    $query = "SELECT * FROM users";
-                                    $select_all_users = mysqli_query($connection, $query);
-                                    $user_count = mysqli_num_rows($select_all_users);
-                                    echo "<div class='huge'>{$user_count}</div>";
-                                    ?>
+                                    <div class='huge'><?= $user_count = recordCount('users'); ?></div>
                                     <div> Users</div>
                                 </div>
                             </div>
@@ -112,12 +96,7 @@
                                     <i class="fa fa-list fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <?php
-                                    $query = "SELECT * FROM categories";
-                                    $select_all_categories = mysqli_query($connection, $query);
-                                    $categories_count = mysqli_num_rows($select_all_categories);
-                                    echo "<div class='huge'>{$categories_count}</div>";
-                                    ?>
+                                    <div class='huge'><?= $categories_count = recordCount('categories'); ?></div>
                                     <div>Categories</div>
                                 </div>
                             </div>

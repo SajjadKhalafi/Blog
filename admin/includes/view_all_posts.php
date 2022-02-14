5c67a3599f38df68f7804f0027400a3f984dd067
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2022-02-14 10:21:02 +0330

    Cloning empty fields and table shifting fix

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -24,20 +24,21 @@ if (isset($_POST['checkboxArray'])) {
                 $select_posts = mysqli_query($connection, $query);
                 confirmQuery($select_posts);
                 while ($row = mysqli_fetch_array($select_posts)){
-                    $post_id = $row['post_id'];
-                    $post_author = $row['post_author'];
-                    $post_title = $row['post_title'];
-                    $post_category_id = $row['post_category_id'];
-                    $post_status = $row['post_status'];
-                    $post_image = $row['post_image'];
-                    $post_tags = $row['post_tags'];
-                    $post_comments_count = $row['post_comment_count'];
-                    $post_date = $row['post_date'];
-                    $post_content = $row['post_content'];
+                    $post_id = escape($row['post_id']);
+                    $post_author = escape($row['post_author']);
+                    $post_user = escape($row['post_user']);
+                    $post_title = escape($row['post_title']);
+                    $post_category_id = escape($row['post_category_id']);
+                    $post_status = escape($row['post_status']);
+                    $post_image = escape($row['post_image']);
+                    $post_tags = escape($row['post_tags']);
+                    $post_comments_count = escape($row['post_comment_count']);
+                    $post_date = escape($row['post_date']);
+                    $post_content = escape($row['post_content']);
                 }
 
-                $query = "INSERT INTO posts(post_category_id , post_title , post_author , post_date , post_image , post_content , post_tags  , post_status)";
-                $query .= " VALUES ($post_category_id , '$post_title' , '$post_author' , now() , '$post_image' , '$post_content' , '$post_tags' , '$post_status')";
+                $query = "INSERT INTO posts(post_category_id , post_title , post_author ,post_user , post_date , post_image , post_content , post_tags  , post_status)";
+                $query .= " VALUES ($post_category_id , '$post_title' , '$post_author' ,'$post_user' , now() , '$post_image' , '$post_content' , '$post_tags' , '$post_status')";
                 $copy_post = mysqli_query($connection, $query);
                 confirmQuery($copy_post);
                 break;
